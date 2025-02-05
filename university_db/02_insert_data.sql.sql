ed65ca80bea2808aa90b72f062e1fef8bf4526a5
KurtIvanMiciano2J <micianokurtivan_bsit@plmun.edu.ph>
2025-02-06 01:53:15 +0800

    Add files via upload

---
+++ university_db/02_insert_data.sql.sql
@@ -0,0 +1,4 @@
+INSERT INTO tblStudents (student_name, student_email, student_phone)  
+VALUES  
+    ('Kurt Miciano', 'kurtpogimax@gmail.com', '09666947659'),  
+    ('Kurt Ivan', 'kurtivanph@gmail.com', '09815110021');
